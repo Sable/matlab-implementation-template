3d2599977e6cc268320f8cd4df3487ff193ec9e8
elavoie <elavoie@users.noreply.github.com>
2016-08-19 18:35:50 +0200

    Update runner.m

--- runner.m
+++ runner.m
@@ -1,10 +1,10 @@
-function runner(parameter)
+function runner(input_size)
 
 % Create or load input data
 %% Use Ostrich random number generator compiled with MEX
 %% and automatically included in the environment of the
 %% ostrich-matlab-environment
-input = createMatrixRandJS(1, parameter);
+input = createMatrixRandJS(1, input_size);
 
 % Run kernel and measure time for core computation 
 tic;
@@ -12,12 +12,12 @@ output = kernel(input);
 elapsedTime  = toc;
 
 % Verify output
-checksum = verify(parameter, input);
+checksum = verify(input_size, input);
 
 % Display output
 disp('{');
 disp('"input":');
-disp(parameter);
+disp(input_size);
 disp(', "time": ');
 disp(elapsedTime);
 disp(', "output": ');
