d90018b28eea336bee1142f2c360cfa64164ece6
elavoie <elavoie@users.noreply.github.com>
2016-08-23 14:07:26 +0200

    Update runner.m

--- runner.m
+++ runner.m
@@ -1,10 +1,19 @@
 function runner(input_size)
 
 % Create or load input data
-%% Use Ostrich random number generator compiled with MEX
-%% and automatically included in the environment of the
+
+% Use the built-in Mersenne-Twister algorithm,
+% the initial state (seed) of the random number generator
+% should be initialized in the environment used
+% (Done by default for ostrich-matlab-environment 
+%  and ostrich-octave-environment)
+input = rand(1, input_size);
+
+%% Deprecated: uncomment the following line to use the
+%% Ostrich random number generator compiled with MEX
+%% also automatically included in the environment of the
 %% ostrich-matlab-environment
-input = createMatrixRandJS(1, input_size);
+%% input = createMatrixRandJS(1, input_size);
 
 % Run kernel and measure time for core computation 
 tic;
